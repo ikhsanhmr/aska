4fec7688cf9b2d75bb9fc560ebacc677e98152e3
ikimamatt <97777118+ikimamatt@users.noreply.github.com>
2025-07-03 09:14:46 +0800

    Revert "add export accesspoint"

--- app/Http/Controllers/Admin/Inventory/AccessPointController.php
+++ app/Http/Controllers/Admin/Inventory/AccessPointController.php
@@ -3,45 +3,76 @@
 namespace App\Http\Controllers\Admin\Inventory;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AccessPointRequest;
 use App\Models\Admin\Inventory\AccessPoint;
+use App\Models\Admin\MasterData\DeviceBrand;
 use App\Models\Admin\MasterData\Region;
-use App\Models\Admin\MasterData\Unit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AccessPointController extends Controller
 {
-    protected $accessPoint;
-    protected $region;
-    protected $unit;
-
-    public function __construct(AccessPoint $accessPoint, Region $region, Unit $unit)
+    private $accessPoint;
+    private $region;
+    private $brand;
+    public function __construct(AccessPoint $accessPoint, Region $region, DeviceBrand $brand)
     {
         $this->accessPoint = $accessPoint;
         $this->region = $region;
-        $this->unit = $unit;
+        $this->brand = $brand;
     }
 
-    public function index(Request $request)
+    function index()
     {
-        $kd_region = $request->query('kd_region');
-        $unit_id = $request->query('unit_id');
+        return view('admin.inventory.access-point.index', ['datas' => $this->accessPoint->getAllData()]);
+    }
 
-        $query = $this->accessPoint->with('region')->latest();
-
-        if ($kd_region) {
-            $query->where('kd_region', $kd_region);
-        }
-
-        if ($unit_id) {
-            $query->where('unit_id', $unit_id);
-        }
-
-        $datas = $query->get();
-
-        return view('admin.inventory.access-point.index', [
-            'datas' => $datas,
+    public function create()
+    {
+        return view('admin.inventory.access-point.create', [
             'regions' => $this->region->getAllData(),
-            'units' => $this->unit->getAllData(),
         ]);
+    }
+
+
+    public function store(AccessPointRequest $request)
+    {
+        try {
+            $this->accessPoint->create($request->validated());
+        } catch (\Exception $e) {
+            dd($e);
+            return redirect()->back()->withInput()->with('error', 'Gagal Tambah Data access point');
+        }
+        return redirect()->route('access-point.index')->with('success', 'Berhasil Tambah Data access point');
+    }
+
+
+    public function show()
+    {
+        //
+    }
+
+
+    public function edit(AccessPoint $access_point)
+    {
+        return view('admin.inventory.access-point.edit', [
+            'accessPoint' => $access_point,
+            'regions' => $this->region->getAllData(),
+        ]);
+    }
+
+
+    public function update(AccessPointRequest $request, AccessPoint $access_point)
+    {
+        $access_point->update($request->validated());
+
+        return redirect()->route('access-point.index')->with('success', 'Berhasil Mengubah Data access point');
+    }
+
+
+    public function destroy(AccessPoint $access_point)
+    {
+        $access_point->delete();
+        return redirect()->route('access-point.index')->with('success', 'Berhasil Hapus Data access point');
     }
 }
