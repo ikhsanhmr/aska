5bd5facc3fed8146d0e8f23b7bc3dd54ce7e6881
syahrilfaizal <endabolehtau123@gmail.com>
2025-07-03 08:51:33 +0800

    menambahkan filter dan export pada inventory cctv

--- resources/views/admin/inventory/cctv/index.blade.php
+++ resources/views/admin/inventory/cctv/index.blade.php
@@ -17,9 +17,19 @@
         <h4 class="mb-3 mb-md-0">Data CCTV</h4>
     </div>
     <div class="d-flex align-items-center flex-wrap text-nowrap">
-        <a href="{{route('cctv.create')}}" class="btn btn-info btn-icon-text mb-2 mb-md-0">
+        <a href="{{route('cctv.create')}}" class="btn btn-info btn-icon-text mb-2 mb-md-0 me-2">
             <i class="btn-icon-prepend" data-feather="plus"></i>
             Tambah Data
+        </a>
+        {{-- Tombol Export Excel --}}
+        <a href="{{ route('cctv.export.excel', request()->query()) }}" class="btn btn-success btn-icon-text mb-2 mb-md-0 me-2">
+            <i class="btn-icon-prepend" data-feather="file-text"></i>
+            Export Excel
+        </a>
+        {{-- Tombol Export PDF --}}
+        <a href="{{ route('cctv.export.pdf', request()->query()) }}" class="btn btn-danger btn-icon-text mb-2 mb-md-0">
+            <i class="btn-icon-prepend" data-feather="file"></i>
+            Export PDF
         </a>
     </div>
 </div>
@@ -29,6 +39,29 @@
         <div class="card">
             <div class="card-body">
                 <h6 class="card-title">Data CCTV</h6>
+                {{-- FORM FILTER --}}
+                <form action="{{ route('cctv.index') }}" method="GET">
+                    <div class="row">
+                        <div class="col-md-5">
+                            <div class="mb-3">
+                                <select class="form-select select2" name="kd_region" id="kd_region">
+                                    <option value="">-- Tampilkan Semua Unit --</option>
+                                    @foreach ($regions as $region)
+                                        <option value="{{ $region->kd_region }}" {{ request('kd_region') == $region->kd_region ? 'selected' : '' }}>
+                                            {{ $region->nama_region }} ({{ $region->kd_region }})
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-4 align-self-end">
+                            <div class="mb-3">
+                                <button type="submit" class="btn btn-primary">Filter</button>
+                                <a href="{{ route('cctv.index') }}" class="btn btn-secondary">Reset</a>
+                            </div>
+                        </div>
+                    </div>
+                </form>
                 <div class="table-responsive">
                     <table id="dataTableExample" class="table">
                         <thead>
